af667acb012b2828eab621c17c1cbb6f9ae3bfef
Ananth69420 <76115580+Ananth69420@users.noreply.github.com>
2025-06-30 21:28:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
 
-const letter = `my baby, my fav human, darling, honey, bunny, babyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy,
+const letter = `i honestly don’t even remember how exactly it all started, but what i do know is that it’s one of the best things to happen in my life. from random convos in college to canteen fights to awkward flirting to full-blown chaos, somehow we ended up here. and i’m so glad we did.
 
-i still remember when you came up to me in college and introduced yourself. i didn’t expect it, but that small moment became something really big in my life. at first, we just talked a little, had some canteen moments which went bad on the 2nd or 3rd time (idk) , and slowly we started talking more, meeting more and that’s when everything started changing.
+i know i mess up a lot. i say dumb things, i get moody, i act distant sometimes, and i know it hurts you. but please never think it’s because i don’t care. i care about you more than i’ve ever cared about anyone. even when i’m acting weird or quiet, you’re the one person who’s always on my mind.
 
-i liked you from the start, honestly. but somewhere in the middle i lost that spark for a bit cuz uk why but once we started hanging out properly again, everything came back, and this time it hit harder. i started feeling things for you in a way i’ve never felt for anyone else.
+you’ve seriously changed how i see relationships. i used to not care much about all that couple stuff, but now it actually means something to me. and that’s because of you.
 
-on may 7th, when we fought (idk over what lol), and i asked if you loved me, i didn’t expect a yes. but you said it and that moment just made me feel like the luckiest guy ever. 
+i love how you hype me up, how you match my energy, how you bully me one second and act soft the next. even when you’re threatening to steal my salary or make me go broke after marriage, i still want all of it with you.
 
-we fight a lot, yeah. and most of the time it’s because of me. i mess up, i say dumb things, and i act distant sometimes but please know, it’s never because i don’t care. i care so much. more than i show, more than i know how to express. and i’m sorry for the times i made you feel bad or ignored.
+you’re the most beautiful, chaotic, caring, stubborn, dramatic and lovable human i know. and i wouldn’t want anyone else by my side.
 
-you’re the most beautiful, breathtaking, cutest girl i’ve ever seen. like seriously . your smile, your voice, your angry face (yes even that), your whole vibe it’s all perfect to me.
+i can’t wait for the day when we get to be together properly. no hostel, no distance, no hiding. just us. i’ll come back home tired, and you’ll be waiting to yell at me or cuddle or both. and that’s the version of life i actually want.
 
-i wish i could spoil you the way i want to. buy you your fav things, take you out, give you surprises but you know how things are for me right now. still, i promise i’ll work hard, get better, and one day give you the life you deserve.
-
-you’ve changed the way i see love. before you, i didn’t believe in all this relationship stuff either. but now? now i can’t imagine a day without talking to you, without hearing your voice. you make even my worst days feel okay.
-
-i want a future with you. the kind where we come home tired, eat together, cuddle, have fucking sex sex, talk about random stuff and fall asleep knowing we’ve got each other. i don’t care about anything else as long as it’s you beside me.
+i love you. so much. more than you probably realise. and no matter how things go, i’m not going anywhere. unless you kill me, which is a real possibility considering how you talk sometimes.
 
 i love you so so so so so soo so soos oso ososoo ossoso osoososo sosos ososos soooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo much.
 `;
